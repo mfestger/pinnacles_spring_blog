8d17c4fc9d0525f5687edd2dd37861b5385b0bb6
Mike Festger <festger@gmail.com>
2017-06-19 11:32:12 -0500

    Completed first exercise using the Spring framework

--- src/main/java/com/codeup/controllers/MathController.java
+++ src/main/java/com/codeup/controllers/MathController.java
@@ -2,19 +2,35 @@ package com.codeup.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MathController {
 
-    @GetMapping("/add/3/and/4")
+    @GetMapping("/add/{firstNumber}/and/{secondNumber}")
     @ResponseBody
-    public String example1(@PathVariable int firstNumber, @PathVariable int secondNumber) {
-        firstNumber = 3;
-        secondNumber = 4;
-        int result = firstNumber + secondNumber;
-        return String.format("The result is" + result);
+    public int add(@PathVariable int firstNumber, @PathVariable int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    @GetMapping("/subtract/{firstNumber}/from/{secondNumber}")
+    @ResponseBody
+    public int subtract(@PathVariable int firstNumber, @PathVariable int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    @GetMapping("/multiply/{firstNumber}/and/{secondNumber}")
+    @ResponseBody
+    public int multiply(@PathVariable int firstNumber, @PathVariable int secondNumber){
+        return firstNumber * secondNumber;
+    }
+
+    @GetMapping("/divide/{firstNumber}/by/{secondNumber}")
+    @ResponseBody
+    public double divide(@PathVariable int firstNumber, @PathVariable int secondNumber) {
+        return (double) firstNumber / secondNumber;
     }
 
 
