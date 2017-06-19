d446d2e1c315862996552f466ea39f4028e7bc5d
Mike Festger <festger@gmail.com>
2017-06-19 15:39:27 -0500

    The requirements have mostly been met, this commit is save my progress.
    Currently the url path works roll-dice/3 for example would return what the user entered versus the random number generated by the dice.
    
    TO DO:
    
     - Make it so the links on the homepage direct the user to said location

--- src/main/java/com/codeup/controllers/RollDiceController.java
+++ src/main/java/com/codeup/controllers/RollDiceController.java
@@ -1,4 +1,29 @@
 package com.codeup.controllers;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import java.util.Random;
+
+@Controller
 public class RollDiceController {
+
+    @GetMapping("/roll-dice")
+    public String rollDice() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{userGuess}")
+    @ResponseBody
+    public String Guess(@PathVariable int userGuess, Model model) {
+        model.addAttribute("userGuess", userGuess);
+        Random randomGenerator = new Random();
+        int randomInt = randomGenerator.nextInt(6);
+        return "Your guess was: " + userGuess + " and the correct number was: " + randomInt;
+    }
+
+
 }
