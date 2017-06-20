954dedf3631c2be82846d69a5284f1f2a3a053b3
Mike Festger <festger@gmail.com>
2017-06-20 14:09:45 -0500

    Initial Commit for PostSvc.java

---
+++ src/main/java/com/codeup/svcs/PostSvc.java
@@ -0,0 +1,14 @@
+package com.codeup.svcs;
+import com.codeup.models.Ad;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostSvc {
+
+   // @Service("postSvc")
+    private List<Ad> ads;
+
+
+
+}
